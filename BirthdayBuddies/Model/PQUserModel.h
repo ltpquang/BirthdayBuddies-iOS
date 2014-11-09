abab26f338dbaf4bb092a5ea8d980494de1c41f6
Quang Le <phuc.quang102@gmail.com>
2014-11-09 12:06:10 +0700

    Basic user model

---
+++ BirthdayBuddies/Model/PQUserModel.h
@@ -0,0 +1,13 @@
+//
+//  PQUserModel.h
+//  BirthdayBuddies
+//
+//  Created by Le Thai Phuc Quang on 10/31/14.
+//  Copyright (c) 2014 Le Thai Phuc Quang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PQUserModel : NSObject
+
+@end
---
+++ BirthdayBuddies/Model/PQUserModel.m
@@ -0,0 +1,27 @@
+//
+//  PQUserModel.m
+//  BirthdayBuddies
+//
+//  Created by Le Thai Phuc Quang on 10/31/14.
+//  Copyright (c) 2014 Le Thai Phuc Quang. All rights reserved.
+//
+
+#import "PQUserModel.h"
+
+@interface PQUserModel()
+
+@property NSString* userId;
+@property NSString* userFacebookId;
+@property NSString* userName;
+@property int userDay;
+@property int userMonth;
+@property int userYear;
+
+
+@end
+
+@implementation PQUserModel
+
+
+
+@end
