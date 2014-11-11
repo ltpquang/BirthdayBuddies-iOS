3a02dab97b234696f3e264eef52479b19ccccd76
Quang Le <phuc.quang102@gmail.com>
2014-11-11 12:45:31 +0700

    Add fields to User model, define Zodiac enum

---
+++ BirthdayBuddies/Enum/PQEnum.h
@@ -0,0 +1,13 @@
+//
+//  PQEnum.h
+//  BirthdayBuddies
+//
+//  Created by Le Thai Phuc Quang on 11/11/14.
+//  Copyright (c) 2014 Le Thai Phuc Quang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PQEnum : NSObject
+
+@end
